0af47409925d40eca85cec1158a38322631e6078
Anand Zutshi <anandzutshi@Anands-MacBook-Pro.local>
2019-03-03 01:12:14 +0530

    login and signup added

--- feature/src/main/java/com/sih/android/accenttranslatorvoip/feature/LoginActivity.java
+++ feature/src/main/java/com/sih/android/accenttranslatorvoip/feature/LoginActivity.java
@@ -2,9 +2,14 @@ package com.sih.android.accenttranslatorvoip.feature;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.content.SharedPreferences;
+import android.widget.Toast;
+import android.view.View;
+import android.content.Intent;
 
 import org.w3c.dom.Text;
 
@@ -16,6 +21,9 @@ public class LoginActivity extends AppCompatActivity {
     private EditText loginUsername;
     private Button loginSubmit;
     private Button loginSignup;
+    private static final String PREFER_NAME = "Reg";
+    SharedPreferences sharedPreferences;
+    UserSession session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,88 @@ public class LoginActivity extends AppCompatActivity {
         loginSubmit = (Button)findViewById(R.id.login_submit);
         loginSignup = (Button)findViewById(R.id.login_signup);
 
+        sharedPreferences = getApplicationContext().getSharedPreferences("Reg", 0);
 
+        session = new UserSession(getApplicationContext());
+
+        Toast.makeText(getApplicationContext(),
+                "User Login Status: " + session.isUserLoggedIn(),
+                Toast.LENGTH_LONG).show();
+
+        sharedPreferences = getSharedPreferences(PREFER_NAME, Context.MODE_PRIVATE);
+
+        loginSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new  Intent(getApplicationContext(),SignupActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                // Add new Flag to start new Activity
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+
+                finish();
+            }
+        });
+
+        loginSubmit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // Get username, password from EditText
+                String username = loginUsername.getText().toString();
+                String password = loginPassword.getText().toString();
+
+                // Validate if username, password is filled
+                if(username.trim().length() > 0 && password.trim().length() > 0){
+                    String uName = null;
+                    String uPassword =null;
+
+                    if (sharedPreferences.contains("Name")) {
+                        uName = sharedPreferences.getString("Name", "");
+
+                    }
+                    if (sharedPreferences.contains("txtPassword")) {
+                        uPassword = sharedPreferences.getString("txtPassword", "");
+
+                    }
+
+                    // Object uName = null;
+                    // Object uEmail = null;
+                    if(username.equals(uName) && password.equals(uPassword)){
+
+                        session.createUserLoginSession(uName,
+                                uPassword);
+
+                        // Starting MainActivity
+                        Intent i = new  Intent(getApplicationContext(),Dashboard.class);
+                        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                        // Add new Flag to start new Activity
+                        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(i);
+
+                        finish();
+
+                    }else{
+
+                        // username / password doesn't match&
+                        Toast.makeText(getApplicationContext(),
+                                "Username/Password is incorrect",
+                                Toast.LENGTH_LONG).show();
+
+                    }
+                }else{
+
+                    // user didn't entered username or password
+                    Toast.makeText(getApplicationContext(),
+                            "Please enter username and password",
+                            Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
     }
 }
--- feature/src/main/java/com/sih/android/accenttranslatorvoip/feature/SignupActivity.java
+++ feature/src/main/java/com/sih/android/accenttranslatorvoip/feature/SignupActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.view.View;
+import android.widget.Toast;
+import android.content.Intent;
 
 
 public class SignupActivity extends AppCompatActivity {
@@ -15,6 +20,9 @@ public class SignupActivity extends AppCompatActivity {
     private EditText signupEmail;
     private EditText signupPassword;
     private Button signupSubmit;
+    SharedPreferences sharedPreferences;
+    Editor editor;
+    UserSession session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,42 @@ public class SignupActivity extends AppCompatActivity {
         signupEmail = (EditText)findViewById(R.id.signup_email);
         signupPassword = (EditText)findViewById(R.id.signup_password);
         signupSubmit = (Button)findViewById(R.id.signup_submit);
+
+        sharedPreferences = getApplicationContext().getSharedPreferences("Reg", 0);
+// get editor to edit in file
+        editor = sharedPreferences.edit();
+
+        signupSubmit.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick (View v) {
+                String name = signupUsername.getText().toString();
+                String email = signupEmail.getText().toString();
+                String pass = signupPassword.getText().toString();
+
+                if(signupUsername.getText().length()<=0){
+                    Toast.makeText(SignupActivity.this, "Enter name", Toast.LENGTH_SHORT).show();
+                }
+                else if( signupEmail.getText().length()<=0){
+                    Toast.makeText(SignupActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+                }
+                else if( signupPassword.getText().length()<=0){
+                    Toast.makeText(SignupActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
+                }
+                else{
+
+                    // as now we have information in string. Lets stored them with the help of editor
+                    editor.putString("Name", name);
+                    editor.putString("Email",email);
+                    editor.putString("txtPassword",pass);
+                    //editor.commit();
+                }   // commit the values
+
+                // after saving the value open next activity
+                Intent ob = new Intent(SignupActivity.this, Dashboard.class);
+                startActivity(ob);
+
+            }
+        });
 
     }
 }
