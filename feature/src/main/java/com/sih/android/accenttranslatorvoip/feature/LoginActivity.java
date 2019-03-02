dbaea513c020d35ceeb3b220d85066e2864276d3
Anand Zutshi <anandzutshi@Anands-MacBook-Pro.local>
2019-03-02 23:10:39 +0530

    modified activities

--- feature/src/main/java/com/sih/android/accenttranslatorvoip/feature/LoginActivity.java
+++ feature/src/main/java/com/sih/android/accenttranslatorvoip/feature/LoginActivity.java
@@ -2,12 +2,32 @@ package com.sih.android.accenttranslatorvoip.feature;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    private TextView loginHeading;
+    private EditText loginPassword;
+    private EditText loginUsername;
+    private Button loginSubmit;
+    private Button loginSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginHeading = (TextView)findViewById(R.id.login_heading);
+        loginPassword = (EditText)findViewById(R.id.login_password);
+        loginUsername = (EditText)findViewById(R.id.login_username);
+        loginSubmit = (Button)findViewById(R.id.login_submit);
+        loginSignup = (Button)findViewById(R.id.login_signup);
+
+
     }
 }
--- feature/src/main/java/com/sih/android/accenttranslatorvoip/feature/SignupActivity.java
+++ feature/src/main/java/com/sih/android/accenttranslatorvoip/feature/SignupActivity.java
@@ -2,12 +2,30 @@ package com.sih.android.accenttranslatorvoip.feature;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class SignupActivity extends AppCompatActivity {
+
+
+    private TextView signupHeading;
+    private EditText signupUsername;
+    private EditText signupEmail;
+    private EditText signupPassword;
+    private Button signupSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        signupHeading = (TextView)findViewById(R.id.signup_heading);
+        signupUsername = (EditText)findViewById(R.id.signup_username);
+        signupEmail = (EditText)findViewById(R.id.signup_email);
+        signupPassword = (EditText)findViewById(R.id.signup_password);
+        signupSubmit = (Button)findViewById(R.id.signup_submit);
+
     }
 }
